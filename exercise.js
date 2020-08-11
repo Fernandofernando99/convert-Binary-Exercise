cdd435a99726467163f8eb1e0f9383857c8203c6
Fernandofernando99 <69098094+Fernandofernando99@users.noreply.github.com>
2020-08-11 09:07:13 +0700

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -1,3 +1,13 @@
+/* Algoritma 
+1. Membuat variabel untuk menampung angka binary
+2. Membuat variabel untuk menampung kata binary yang berasal dari konversi angka binary ke string
+3. Membuat variabel untuk menampung kata desimal
+4. Membuat variabel untuk menampung angka desimal
+5. Mengambil digit paling kanan dari angka binary satu per satu sampai dengan angka binary paling kiri dan dimasukkan ke variabel kata desimal
+6. Melakukan perhitungan dari kata desimal dimana setiap digit akan dikalikan dengan 2 pangkat dari urutan digit dan setiap hasil perkaliannya ditambahkan
+*/
+
+
 let numBinaary = 11111;
 let binaryString = numBinaary.toString();
 let decimalString = "";
